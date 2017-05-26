1929c331fe2bb675b56895c629d1fefd7f2d899e
Imran Ali <imranal@math.uio.no>
2017-05-26 17:23:18 +0200

    by constanly having diarrhea

--- ppu3220/FoU_Oppgaven/figures/score.py
+++ ppu3220/FoU_Oppgaven/figures/score.py
@@ -1,8 +1,8 @@
 import matplotlib.pylab as plt
 import matplotlib.ticker as ticker
 
-x = [0, 0.5, 1, 2, 3, 3.5, 4, 4.5, 5, 7.5, 8.5,9]
-y = [1,2,2,3,1,2,2,2,2,1,1,0]
+x = [0, 0.5, 1, 2, 3, 3.5, 4, 4.5, 5, 7.5, 8.5, 9]
+y = [2,  2,  2, 3, 1,  2,  2,  2,  2,  1,   1,  0]
 
 fig, ax = plt.subplots(1)
 
@@ -15,3 +15,9 @@ ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
 
 plt.show()
+
+avg = 0
+for i in range(len(x) - 1):
+    avg += x[i]*y[i]
+avg = avg/sum(y)
+print(avg)
